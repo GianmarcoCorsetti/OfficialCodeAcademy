e883607234fab146bfd09c72d8999ee5e95292aa
GianmarcoCorsetti <gianmarco.corsetti97@gmail.com>
2021-10-19 11:51:11 +0200

    Punto di partenza

---
+++ CodeAcademyWeb/Controllers/CourseController.cs
@@ -0,0 +1,65 @@
+﻿using AcademyModel.Entities;
+using AcademyModel.Services;
+using AutoMapper;
+using CodeAcademyWeb.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeAcademyWeb.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CourseController : Controller
+	{
+		private IDidactisService service;
+		private IMapper mapper;
+		public CourseController(IDidactisService service, IMapper mapper)
+		{
+			this.service = service;
+			this.mapper = mapper;
+		}
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var course = service.GetAllCourses();
+			var courseDTOs = mapper.Map<IEnumerable<CourseDTO>>(course);
+			return Ok(courseDTOs);
+		}
+		[HttpGet]
+		[Route("{id}")]
+		public IActionResult GetById(long id){
+			var course = service.GetCourseById(id);
+			var courseDTO = mapper.Map<CourseDTO>(course);
+			return Ok(courseDTO);
+		}
+	
+		[HttpGet]
+		[Route("areas")]
+		public IActionResult GetAllAreas()
+		{
+			var areas = service.GetAllAreas();
+			var areaDTOs = mapper.Map<IEnumerable<AreaDTO>>(areas);
+			return Ok(areaDTOs);
+		}
+
+		public IActionResult GetLastNCurses(int n)
+		{
+			var courses = service.GetLastCourses(n);
+
+			var courseDTOs = mapper.Map<IEnumerable<CourseDTO>>(courses);
+			return Ok(courseDTOs);
+		}
+		[HttpPost]
+		public IActionResult CreateCourse(CourseDTO courseDTO)
+        {
+            var course = mapper.Map<Course>(courseDTO);
+			course = service.CreateCourse(course);
+			var createdDTO = mapper.Map<CourseDTO>(course);
+			return Created($"api/course/{createdDTO.Id}", createdDTO);
+        }
+	}
+}
